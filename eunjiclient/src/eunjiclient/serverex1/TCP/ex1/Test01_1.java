2223736e23eb3d23fe933cf04b6160e5e2db82fb
은지프로젝트 <dmswlwnsgk@gmail.com>
2023-07-17 17:56:24 +0900

    111

---
+++ eunjiclient/src/eunjiclient/serverex1/TCP/ex1/Test01_1.java
@@ -0,0 +1,18 @@
+package eunjiclient.serverex1.TCP.ex1;
+
+import java.net.ServerSocket;
+
+public class Test01_1 {
+	
+	public static void main(String[] args) throws Exception{
+		System.out.println("서버 실행 중...");
+		
+		//1)서버 소켓 만들기
+		ServerSocket serverSocket = new ServerSocket(8888);
+		
+		
+		
+
+	}
+
+}
